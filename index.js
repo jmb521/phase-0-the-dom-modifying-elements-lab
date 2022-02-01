78a3f2d81593aadff2b5223cb7e79ba77ddedf92
Jennifer Pazos <jmp521@gmail.com>
2022-02-01 15:02:34 -0600

    commit tree faa2a4611b026779588dbd25ddaf992523db1c40

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-// Write your code here!
+let newHeader = document.createElement("h1")
+newHeader.id = "victory"
+newHeader.textContent = "Jennifer is the champion"
+document.body.append(newHeader)
+const main = document.getElementById("main")
+main.remove()
+console.log("main should be gone", main)
